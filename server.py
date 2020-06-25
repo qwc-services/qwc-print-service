9a6a465a79b98c97bc10a1a927fe9467ce560958
Hussein Kabbout <hussein.kabbout@sourcepole.ch>
2020-06-25 17:04:29 +0200

    Replace flask_restplus with flask_restx https://github.com/qwc-services/qwc-services-core/issues/2

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, request, Response, stream_with_context, \
     jsonify
-from flask_restplus import Api, Resource
+from flask_restx import Api, Resource
 from flask_jwt_extended import jwt_optional, get_jwt_identity
 import os
 import requests
